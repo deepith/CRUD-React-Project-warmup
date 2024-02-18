89e0b44c07094faf53235e353063cb9645033cb9
dkfalcon <deepithkunder@gmail.com>
2024-02-18 17:32:49 +0530

    deploy2

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Context>
-    <BrowserRouter>
+    <BrowserRouter basename={"/CRUD-React-Project-warmup/"}>
       <App />
       <ToastContainer />
     </BrowserRouter>
